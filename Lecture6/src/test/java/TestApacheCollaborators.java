53f4d3dae7948f81ecf03621a0d76abd3a2d629e
vitalyp <vit.plisko@gmail.com>
2018-12-21 15:43:50 +0300

    lecture6 refactoring

--- Lecture6/src/test/java/TestApacheCollaborators.java
+++ Lecture6/src/test/java/TestApacheCollaborators.java
@@ -1,5 +1,6 @@
 import helper.HelperApacheHttpComponents;
 import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
 import org.testng.Assert;
@@ -9,18 +10,20 @@ import user.User;
 import java.io.IOException;
 import java.util.List;
 
-public class CollaboratorsApache {
+public class TestApacheCollaborators {
     @Test
     public void getCollaborators() throws IOException {
         HttpRequest httpRequest = new HttpGet("/repos/pliskov/AutomationSchool/collaborators");
-        List<User> userList = HelperApacheHttpComponents.getUsers(httpRequest);
+        CloseableHttpResponse response = HelperApacheHttpComponents.sendRequest(httpRequest);
+        List<User> userList = HelperApacheHttpComponents.getUsers(response);
         HelperApacheHttpComponents.printUsers(userList);
     }
 
     @Test
     public void addCollaborator() throws IOException {
         HttpRequest httpRequest = new HttpPut("/repos/pliskov/AutomationSchool/collaborators/fabjke");
-        int statusCode = HelperApacheHttpComponents.getResponseStatusCode(httpRequest);
+        CloseableHttpResponse response = HelperApacheHttpComponents.sendRequest(httpRequest);
+        int statusCode = HelperApacheHttpComponents.getResponseStatusCode(response);
         int successCode = 201;
         if (statusCode == successCode) {
             System.out.println("The invite has been successfully sent");
--- Lecture6/src/test/java/TestRestAssuredCollaborators.java
+++ Lecture6/src/test/java/TestRestAssuredCollaborators.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.preemptive;
 
-public class MyGithubRepoCollaborators {
+public class TestRestAssuredCollaborators {
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = "https://api.github.com";
